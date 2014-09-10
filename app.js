2fd0fa1257e8c766cd0d693d270af5984fc66e99
Marie Battestini <mbattest@gmail.com>
2014-09-10 15:11:07 +0200

    Added /tags REST API

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var tags = require('./routes/tags');
 
+// MongoDB Connection 
+mongoose.connect('mongodb://localhost/tag_tracker');
 var app = express();
 
 // view engine setup
@@ -24,6 +28,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.get('/tags', tags.index);
+app.get('/tags/:id', tags.show);
+app.post('/tags', tags.create);
+app.put('/tags', tags.update);
+app.delete('/tags', tags.delete);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
